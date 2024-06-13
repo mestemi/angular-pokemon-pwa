6206edcb8261921275d8933f58aec6a6e08ea750
Macià Estela Miarnau <mestemi@uoc.edu>
2024-06-13 02:19:01 +0200

    use routerLink instead of href

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CardDto } from '../../models/card.dto';
-import {MatCardModule} from '@angular/material/card';
+import {MatCardModule, } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, RouterModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
